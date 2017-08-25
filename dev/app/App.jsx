d3efec26de8112ad8d9ed77e76e824e621eb32dd
Dave Fancher <dave@davefancher.com>
2017-08-25 02:08:38 -0400

    Started building routing and outermost components

--- dev/app/App.jsx
+++ dev/app/App.jsx
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
+import Home from "../features/home/Home.jsx";
+import CharacterList from "../features/characters/CharacterList.jsx";
+import Character from "../features/characters/Character.jsx";
 
 export default class App extends Component {
     render () {
         return (
-            <div>Hello</div>
+            <BrowserRouter>
+                <div>
+                    <ul>
+                        <li><NavLink to="/">Home</NavLink></li>
+                        <li><NavLink to="/characters">Characters</NavLink></li>
+                    </ul>
+
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/characters" render={() =>
+                            <div>
+                                <Route exact path={this.props.path} component={CharacterList} />
+                                <Route path={this.props.path + ":id"} component={Character} />
+                            </div>
+                        } />
+                    </Switch>
+                </div>
+            </BrowserRouter>
         );
     }
 }
--- dev/index.jsx
+++ dev/index.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router, Route, Switch } from "react-router"
 
 import App from "./app/App.jsx"
 
-ReactDOM.render(<App />, document.getElementById("container"));
+ReactDOM.render(
+    <App />,
+    document.getElementById("container")
+);
