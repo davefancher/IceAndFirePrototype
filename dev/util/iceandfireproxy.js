d32de1bea44facf24433cb10908e65ef63cd463d
Dave Fancher <dave@davefancher.com>
2017-08-30 00:18:18 -0400

    Standardized caching

--- dev/util/iceandfireproxy.js
+++ dev/util/iceandfireproxy.js
@@ -6,63 +6,68 @@ var apiUrl = "https://www.anapioficeandfire.com/api/";
 
 var reHeaderLink = /^<(http(?:s)?:\/\/[a-zA-Z0-9-_\.]+\.[a-zA-Z0-9]{2,}\/[-a-zA-Z0-9%_\.#//]*[-a-zA-Z0-9:%_\+.~#?&//=]*)>; rel=\"([a-zA-Z]+)\"/;
 
-export function getCharacters(search) {
-    var qs = querystring.stringify(search);
-    var requestUrl = apiUrl + "characters?" + qs;
-
-    var cachedItem = localStorage.getItem(requestUrl);
+function getCachedItem(key, getter) {
+    var cachedItem = localStorage.getItem(key);
 
     if(cachedItem) {
         return (
             new Promise(
-                (resolve, reject) => {
-                    resolve(JSON.parse(cachedItem));
-                }
+                (resolve) => resolve(JSON.parse(cachedItem))
             )
-        );
+        )
     }
 
     return (
-        axios.get(
-            requestUrl,
-            { headers: {
-                "accept": "application/vnd.anapioficeandfire+json; version=1"
-            }
-        }).then(response => {
-            var characters =
-                response
-                    .data
-                    .map(c => ({
-                        id: (() => { var parts = c.url.split("/"); return parts[parts.length - 1]; })(),
-                        name: c.name || c.aliases[0],
-                        gender: c.gender,
-                        culture: c.culture,
-                        born: c.born,
-                        died: c.died
-                    }));
-
-            var paging =
-                response
-                    .headers
-                    .link
-                    .split(", ")
-                    .map(item => reHeaderLink.exec(item))
-                    .reduce((o, match) => {
-                        var url = match[1];
-                        var pageInfo = querystring.parse(url.split("?")[1]);
-                        pageInfo.url = url;
-                        o[match[2]] = pageInfo;
-                        return o;
-                    }, { currentPage: search.page, pageSize: search.pageSize });
-
-            var data = {
-                characters: characters,
-                paging: paging
-            };
-
-            localStorage.setItem(requestUrl, JSON.stringify(data));
-
-            return data;
+        getter()
+            .then(response => {
+                localStorage.setItem(key, JSON.stringify(response));
+                return response;
         })
     );
+}
+
+export function getCharacters(search) {
+    var qs = querystring.stringify(search);
+    var requestUrl = apiUrl + "characters?" + qs;
+
+    return getCachedItem(
+        requestUrl,
+        () =>
+            axios.get(
+                requestUrl,
+                { headers: {
+                    "accept": "application/vnd.anapioficeandfire+json; version=1"
+                }
+            }).then(response => {
+                var characters =
+                    response
+                        .data
+                        .map(c => ({
+                            id: (() => { var parts = c.url.split("/"); return parts[parts.length - 1]; })(),
+                            name: c.name || c.aliases[0],
+                            gender: c.gender,
+                            culture: c.culture,
+                            born: c.born,
+                            died: c.died
+                        }));
+    
+                var paging =
+                    response
+                        .headers
+                        .link
+                        .split(", ")
+                        .map(item => reHeaderLink.exec(item))
+                        .reduce((o, match) => {
+                            var url = match[1];
+                            var pageInfo = querystring.parse(url.split("?")[1]);
+                            pageInfo.url = url;
+                            o[match[2]] = pageInfo;
+                            return o;
+                        }, { currentPage: search.page, pageSize: search.pageSize });
+    
+                return {
+                    characters: characters,
+                    paging: paging
+                };
+            }));
 }
